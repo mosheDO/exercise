2f6656f147ef99e4d22c7119a63f6f1c97911264
moshedo <moshedoppelt@gmail.com>
2020-10-23 09:44:28 +0300

    init stage

---
+++ src/main/java/com/exercise/StringUtil.java
@@ -0,0 +1,15 @@
+package com.exercise;
+
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+public class StringUtil {
+
+
+    public static String[] StringSplit(String s) {
+      
+        throw new NotImplementedException();
+        //Write your code here
+    }
+
+
+}
---
+++ src/test/java/com/exercise/StringUtilTest.java
@@ -0,0 +1,32 @@
+package com.exercise;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+// import org.junit.Assert.assertEquals;
+// import org.junit.runners.JUnit4;
+// import java.util.Arrays;
+
+
+public class StringUtilTest {
+
+    //#region StringSplit
+    @Test
+    public void testEvenStringStringSplit() {
+       String s = "abcdef";
+       String s1 = "HelloWorld";
+       assertEquals("Should handle even string","[ab, cd, ef]", Arrays.toString(StringSplit.solution(s)));
+       assertEquals("Should handle even string","[He, ll, oW, or, ld]", Arrays.toString(StringSplit.solution(s1)));
+    }
+    
+    @Test
+    public void testOddStringStringSplit() {
+       String s = "abcde";
+       String s1 = "LovePizza";
+       assertEquals("Should handle odd string","[ab, cd, e_]", Arrays.toString(StringSplit.solution(s)));
+       assertEquals("Should handle odd string","[Lo, ve, Pi, zz, a_]", Arrays.toString(StringSplit.solution(s1)));
+    }
+
+    //#endregion StringSplit
+}
